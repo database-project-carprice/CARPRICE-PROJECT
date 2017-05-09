4c1c0a26e76900729965e0dae9d392d23830a477
Rakkan Jintasatien <rakkan.j@ku.th>
2017-05-09 12:54:33 +0700

    add-db-function

--- Rentalservice/dbFunction.php
+++ Rentalservice/dbFunction.php
@@ -6,6 +6,8 @@
     echo $_SESSION['end_date']."\n";
     echo $_SESSION['start_time']."\n";
     echo $_SESSION['end_time']."\n";
+    echo $_SESSION['card_type']."\n";
+    echo $_SESSION['card_id']."\n";
     echo $_SESSION['name']."\n";
     echo $_SESSION['lastname']."\n";
     echo $_SESSION['birthday']."\n";
@@ -13,4 +15,9 @@
     echo $_SESSION['phone']."\n";
     echo $_SESSION['dln']."\n";
     
+    
+    // include 'insertIntoRentalDB.php';
+    // include 'insertIntoReservationDB.php';
+    // include 'updateCustomerDB.php';
+
 ?>
