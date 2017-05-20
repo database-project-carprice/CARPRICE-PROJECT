47471faa6c9867e7c7234a2cd858999dade26ca1
Ntp Chong <ntpchong_0411@gmail.com>
2017-05-20 22:40:44 +0700

    edit font in content.php

--- Rentalservice/content.php
+++ Rentalservice/content.php
@@ -108,6 +108,7 @@
     <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- Custom Theme Style -->
     <link href="../css/content.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 </head>
 
 <body>
