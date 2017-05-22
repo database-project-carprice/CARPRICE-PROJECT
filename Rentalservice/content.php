c9c5ac2115f0ce5a781958fadd307c6818f1b555
Ntp Chong <ntpchong_0411@gmail.com>
2017-05-22 13:54:23 +0700

    checkout

--- Rentalservice/content.php
+++ Rentalservice/content.php
@@ -127,7 +127,7 @@
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li><a href="/Rentalservice">Home</a></li>
-                    <li><a href="/selectcar/index.html">Select Car</a></li>
+                    <li><a href="/selectcar/index.html">Show Car</a></li>
                     <li class="active"><a href="/history/index.html">History</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
